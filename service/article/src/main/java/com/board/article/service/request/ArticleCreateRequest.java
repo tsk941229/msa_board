84a7e153368f54bebee6d037d1c31f652545305d
taeseok <tsk941229@gmail.com>
2025-12-05 14:47:49 +0900

    feat: [섹션2] 13. 게시글 CRUD API 구현

---
+++ service/article/src/main/java/com/board/article/service/request/ArticleCreateRequest.java
@@ -0,0 +1,15 @@
+package com.board.article.service.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ArticleCreateRequest {
+
+    private String title;
+    private String content;
+    private Long boardId;
+    private Long writerId;
+
+}
---
+++ service/article/src/main/java/com/board/article/service/request/ArticleUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.board.article.service.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ArticleUpdateRequest {
+
+    private String title;
+    private String content;
+
+}
