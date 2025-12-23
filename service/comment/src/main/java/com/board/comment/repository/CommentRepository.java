943f39259555682e467a982a1850876e3b0b814f
taeseok <tsk941229@gmail.com>
2025-12-23 22:32:45 +0900

    feat: 댓글 최대 2depth 서비스 테스트

--- service/comment/src/main/java/com/board/comment/repository/CommentRepository.java
+++ service/comment/src/main/java/com/board/comment/repository/CommentRepository.java
@@ -13,6 +13,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
            value = "select count(*) from (" +
                    "    select comment_id from comment " +
                    "    where article_id = :articleId and parent_comment_id = :parentCommentId " +
+                   "    limit :limit" +
                    ") t",
            nativeQuery = true
     )
